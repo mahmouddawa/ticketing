e82b6e124553a8160509cd80b28862165ae099c5
Mahmoud Dawa <mahmouddawa990@gmail.com>
2021-07-04 23:05:34 +0200

    the env variable for local and prod + test for update ticket

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -3,16 +3,22 @@ import axios from 'axios';
 const buildClinet = ({ req }) => {
   if (typeof window === "undefined") {
     // We are on the server
+    
+    if(process.env.NODE_ENV === "production"){
+      return axios.create({
+        baseURL:
+          "http://www.ticketing-appme.xyz/",
+        headers: req.headers,
+        });
+    }else{
+      return axios.create({
+        baseURL:
+          //this is local //
+           "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
+        headers: req.headers,
+        });
+    }
 
-    return axios.create({
-      baseURL:
-        //this is local //
-         "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
-        //this is production:
-        //"http://www.ticketing-appme.xyz/",
-
-      headers: req.headers,
-    });
   } else {
     // We must be on the browser
     return axios.create({
